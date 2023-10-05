5a3dcf5c259219c54c1777081d1860b1529ed18a
HARI-PRMD <haripramodb@gmail.com>
2023-10-05 14:33:42 +1100

    Updated Ping command and npm issues

--- src/interactions/ping.ts
+++ src/interactions/ping.ts
@@ -1,20 +1,23 @@
 import { CommandInteraction, SlashCommandBuilder } from "discord.js";
 import { client } from "..";
+import { differenceInMilliseconds } from "date-fns/fp";
 
 export const data = new SlashCommandBuilder()
     .setName("ping")
     .setDescription("returns status of bot's functioning!");
 
 export async function execute(interaction: CommandInteraction) {
+    const startTime = Date.now();
     const sent = await interaction.reply({
         content: "Pinging...",
         fetchReply: true,
     });
+    const endTime = Date.now();
+
+    const heartbeatLatency = client.ws.ping;
+    const roundtripLatency = differenceInMilliseconds(startTime, endTime);
+
     return interaction.editReply(
-        `Bot is Working\n**Websocket heartbeat:** ${
-            client.ws.ping
-        }ms.\nRoundtrip latency: ${
-            sent.createdTimestamp - interaction.createdTimestamp
-        }ms`
+        `Bot is Working\n**Websocket heartbeat:** ${heartbeatLatency}ms.\nRoundtrip latency: ${roundtripLatency}ms`
     );
 }
